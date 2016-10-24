bf34e4dcf226d0d6f79d586215754f60f10a4f45
rosyparmar <rosyparmar1991@gmail.com>
2016-10-23 22:58:49 -0400

    latest-changess

--- neu/webdev/public/assignment/Assignment2/services/widget.service.client.js
+++ neu/webdev/public/assignment/Assignment2/services/widget.service.client.js
@@ -6,7 +6,8 @@
 
     function WidgetService()
     {
-        var widgets = [
+        var widgets =
+            [
             {"_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
             {"_id": "234", "widgetType": "HEADER", "pageId": "235", "size": 4, "text": "Lorem ipsum"},
             {
@@ -17,7 +18,13 @@
                 "_id": "234",
                 "widgetType": "HEADER",
                 "pageId": "234",
-                "text": '<p>Watchmaker <a href="http://gizmodo.com/tag/mbf" rel="nofollow">MB&amp;F</a> isn’t as well-known as  Rolex or Timex, but that’s because the company’s unique creations—like a <a href="http://gizmodo.com/listen-to-an-18-000-tie-fighter-music-box-play-the-sta-1717444112" rel="nofollow">TIE Fighter-shaped music box</a> that plays the <em>Star Wars</em> theme—are made for die-hard collectors. Its latest creation is a <a href="https://www.mbandf.com/en/machines/co-creations/astrograph" target="_blank" rel="noopener">rocket-shaped pen inspired by the moon landing</a>, and I’m desperately trying to justify…<span class="read-more-placeholder"></span><span class="readmore-core-decorated"></span></p>'
+                "text": '<p>Watchmaker <a href="http://gizmodo.com/tag/mbf" rel="nofollow">MB&amp;F</a> ' +
+                'isn’t as well-known as  Rolex or Timex, but that’s because the company’s unique creations—like a' +
+                ' <a href="http://gizmodo.com/listen-to-an-18-000-tie-fighter-music-box-play-the-sta-1717444112" ' +
+                'rel="nofollow">TIE Fighter-shaped music box</a> that plays the <em>Star Wars</em> theme—are made' +
+                ' for die-hard collectors. Its latest creation is a <a href="https://www.mbandf.com/en/machines/co-creations/astrograph"' +
+                ' target="_blank" rel="noopener">rocket-shaped pen inspired by the moon landing</a>, and I’m desperately trying to justify' +
+                '…<span class="read-more-placeholder"></span><span class="readmore-core-decorated"></span></p>'
             },
             {"_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
             {
@@ -42,7 +49,8 @@
         {
             widget.pageId = pageId;
             var newWidgetId = getRandomInt(0, 10000).toString();
-            if (null === findWidgetById(newWidgetId)) {
+            if (null === findWidgetById(newWidgetId))
+            {
                 widget._id = newWidgetId;
                 widgets.push(widget);
                 return widget;
@@ -93,7 +101,7 @@
                     if (newWidget._id === widgetId)
                     {
                         widgets[widIndex] = newWidget;
-                        return widgets[userIndex];
+                        return widgets[widIndex];
                     }
                     else {
                         return null;
@@ -110,7 +118,7 @@
                     if (newWidget._id === widgetId)
                     {
                         widgets[widIndex] = newWidget;
-                        return widgets[userIndex];
+                        return widgets[widIndex];
                     }
                     else {
                         return null;
@@ -127,7 +135,7 @@
                     if (newWidget._id === widgetId)
                     {
                         widgets[widIndex] = newWidget;
-                        return widgets[userIndex];
+                        return widgets[widIndex];
                     }
                     else
                     {
